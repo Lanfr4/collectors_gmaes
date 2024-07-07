d66b0359fc73e5effaedfcddd1a87b93d9bda831
Sebastiano Lanfranchi <128606572+Lanfr4@users.noreply.github.com>
2024-07-07 21:57:28 +0200

    started working on these file

--- file_in_c/campo_da_gioco_forza_4.c
+++ file_in_c/campo_da_gioco_forza_4.c
@@ -1,4 +1,16 @@
-/**/ 
+/*CAMPO DA GIOCO DEL FORZA 4*/ 
+/*dimensioni del campo
+MATRICE 7*6
+*/
+#include<stdio.h>
+#define N 7
+#define M 6
+
 void campo_da_gioco(){
     
+    char campo_f[N][M];
+    int i=0, j=0;
+
+    stampa_campo();
+
 }
